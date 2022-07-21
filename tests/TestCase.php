a0a8fa83c2ddc01c5cebfff97469d154ece5f153
Simon Karlen <simi.albi@outlook.com>
2022-07-21 15:11:16 +0200

    remove directory again after tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -27,7 +27,7 @@ abstract class TestCase extends \PHPUnit\Framework\TestCase
     protected function tearDown(): void
     {
         parent::tearDown();
-//        FileHelper::removeDirectory(Yii::getAlias('@runtime'));
+        FileHelper::removeDirectory(Yii::getAlias('@runtime'));
         $this->destroyApplication();
     }
 
